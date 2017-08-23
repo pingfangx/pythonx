1fa476b73ac02d3ef972c8e7dc54219efbf58826
pingfangx <pingfangx@pingfangx.com>
2017-08-23 17:01:28 +0800

    将翻译完的文件导出为md格式

--- ToolsX/android_studio_translator/android_studio_translator.py
+++ ToolsX/android_studio_translator/android_studio_translator.py
@@ -30,8 +30,11 @@ class AndroidStudioTranslator:
             ['将中文翻译结果导出为OmegaT数据', self.convert_to_omegat_dict, en_file, cn_modified_shortcut_file,
              'data/project_save.tmx.xml'],
             ['处理keymap文件', self.handle_keymap_file, en_file, keymap_file],
+            ['处理翻译完的keymap文件', self.handle_translated_keymap_file,
+             r"E:\workspace\TranslatorX\AndroidStudio\source\keymap_with_desc_delete_ellipsis.properties",
+             r"E:\workspace\TranslatorX\AndroidStudio\target\keymap_with_desc_delete_ellipsis_zh_CN.properties"],
             ['删除OmegaT翻译记忆文件中的快捷方式', self.delete_shortcut, 'data/project_save2.tmx'],
-            ['删除文件中的省略号', self.delete_ellipsis, 'data/keymap_with_desc2.properties'],
+            ['删除文件中的省略号', self.delete_ellipsis, 'data/result/keymap_with_desc.properties'],
             ['删除OmegaT翻译记忆文件中的省略号', self.delete_ellipsis_of_omegat, 'data/project_save2.tmx'],
         ]
         iox.choose_action(action_list)
@@ -299,6 +302,79 @@ class AndroidStudioTranslator:
                         # 有描述，添加
                         append = '\n\n%s %s' % ('[desc]', desc)
             line = '\n\n[%s] %s%s' % (prefix, line, append)
+            result.append(line)
+        filex.write_lines(result_file, result)
+
+    @staticmethod
+    def handle_translated_keymap_file(en_file, cn_file, result_file=None):
+        """
+        处理翻译完的文件
+        :param en_file: 
+        :param cn_file: 
+        :param result_file: 
+        :return: 
+        """
+        if result_file is None:
+            result_file = filex.get_result_file_name(cn_file, '_final')
+        en_lines = filex.read_lines(en_file)
+        cn_lines = filex.read_lines(cn_file)
+        if cn_lines is None:
+            return
+
+        result = []
+        p_title = re.compile(r'^\[(#+)\]\s?(.*)')
+        p_title2 = re.compile(r'(\n*)(#*\s)(.*)')
+        p_desc = re.compile(r'^\[desc\]\s?(.*)')
+        p_comment = re.compile(r'^#\[(x+)\](.*)')
+        for i in range(len(cn_lines)):
+            line = cn_lines[i].replace('\n', '')
+            if line.startswith('#'):
+                # 以#开头不翻译，取原文
+                line = en_lines[i].replace('\n', '')
+                match = re.match(p_comment, line)
+                if match is not None:
+                    # 注释
+                    start_number = match.group(1)
+                    if len(start_number) == 1:
+                        # 1颗星标记基本注释
+                        replace_result = '  \n译注：%s' % (match.group(2))
+                    else:
+                        # 2星以上为加重注释
+                        last_line = result[-1]
+                        if '★' not in last_line:
+                            last_line_match = re.match(p_title2, last_line)
+                            if last_line_match is not None:
+                                start_replace = '★' * (len(start_number) - 1)
+                                last_line = re.sub(p_title2, r'\1\2*%s\3%s*' % (start_replace, start_replace),
+                                                   last_line)
+                                print('替换【%s】为【%s】' % (result[-1], last_line))
+                                result[-1] = last_line
+                        replace_result = '  \n译注：%s' % (match.group(2))
+                    # print('替换【%s】为【%s】' % (line, replace_result))
+                    line = '%s' % replace_result
+            else:
+                # 不以#开头
+                line = encodex.unicode_str_to_chinese(line)
+                en_line = en_lines[i].replace('\n', '')
+                match = re.match(p_title, line)
+                if match is not None:
+                    # 是标题
+                    en_match = re.match(p_title, en_line)
+                    if len(en_match.group(1)) == 5:
+                        replace_result = '\n\n%s（%s）' % (en_match.group(2), match.group(2))
+                    else:
+                        replace_result = '\n\n%s %s（%s）' % (en_match.group(1), en_match.group(2), match.group(2))
+                    # print('\n替换【%s】为【%s】' % (line, replace_result))
+                    line = '%s' % replace_result
+                else:
+                    desc_cn_match = re.match(p_desc, line)
+                    if desc_cn_match is not None:
+                        # 是描述
+                        desc_en_match = re.match(p_desc, en_line)
+                        replace_result = '  \n%s  \n描述：%s' % (desc_en_match.group(1), desc_cn_match.group(1))
+                        # print('\n描述替换【%s】为【%s】' % (line, replace_result))
+                        line = '%s' % replace_result
+
             result.append(line)
         filex.write_lines(result_file, result)
 
