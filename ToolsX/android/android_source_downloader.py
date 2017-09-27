5f66ef6ba4f1b2c35d3cda61f24325ed9843f12c
pingfangx <pingfangx@pingfangx.com>
2017-09-27 08:01:31 +0800

    添加安卓源码下载器

---
+++ ToolsX/android/android_source_downloader.py
@@ -0,0 +1,92 @@
+import os
+from subprocess import call
+from xml.etree import ElementTree as Et
+
+from xx import iox
+
+
+class AndroidSourceDownloader:
+    """
+    参考:Jeanboydev.《 Windows 环境下载 Android 源码》.http://blog.csdn.net/freekiteyu/article/details/70939672
+    感谢
+    """
+
+    def __init__(self, root_dir, tag, git_path, use_tsinghua=True):
+        """
+        :param root_dir:根目录 
+        :param tag: 要下载的分枝标签
+        :param git_path: git路径，如果配置了环境变量，可以直接使用 git
+        :param use_tsinghua: 是否使用清华的镜像
+        """
+        self.root_dir = root_dir
+        self.manifest_dir = self.root_dir + os.path.sep + 'manifest'
+        self.manifest_file = self.manifest_dir + os.path.sep + 'default.xml'
+        self.tag = tag
+        self.source_root = self.root_dir + os.path.sep + tag
+        self.git_path = git_path
+        if use_tsinghua:
+            self.project_root = 'https://aosp.tuna.tsinghua.edu.cn'
+        else:
+            self.project_root = 'https://android.googlesource.com'
+
+    def main(self):
+        action_list = [
+            ['退出', exit],
+            ['clone manifest', self.download_manifest],
+            ['clone manifest 并显示所有分枝', self.download_manifest, True],
+            ['clone manifest 并切换分枝为 ' + self.tag, self.download_manifest],
+            ['下载安卓源码', self.download_android_source],
+        ]
+        iox.choose_action(action_list)
+
+    def download_manifest(self, show_branch=False, chekcout=False):
+        """下载manifest"""
+        if not os.path.exists(self.root_dir):
+            os.makedirs(self.root_dir)
+        os.chdir(self.root_dir)
+
+        if not os.path.exists(self.manifest_dir):
+            cmd = '%s clone %s/%s.git' % (self.git_path, self.project_root, 'platform/manifest')
+            self.run_cmd(cmd)
+        os.chdir(self.manifest_dir)
+        if show_branch:
+            self.run_cmd('%s branch -a' % self.git_path)
+        if chekcout:
+            cmd = '%s checkout %s' % (self.git_path, self.tag)
+            self.run_cmd(cmd)
+
+    @staticmethod
+    def run_cmd(cmd):
+        print(cmd)
+        call(cmd)
+
+    def download_android_source(self):
+        """ 下载安卓源码 """
+        if not os.path.exists(self.source_root):
+            os.mkdir(self.source_root)
+
+        root = Et.parse(self.manifest_file)
+        project_list = root.findall('project')
+        length = len(project_list)
+        for i in range(length):
+            project = project_list[i]
+            print('clone %d/%d' % (i + 1, length))
+            os.chdir(self.source_root)
+            dir_path = project.attrib['path']
+            last = dir_path.rfind("/")
+            if last != -1:
+                # 最后一个名字由 git 创建，将其截去
+                dir_path = self.source_root + os.path.sep + dir_path[:last]
+                if not os.path.exists(dir_path):
+                    os.makedirs(dir_path)
+                # 切换目录
+                os.chdir(dir_path)
+            # 如果没有/，则是在当前目录
+            name = project.attrib['name']
+            cmd = '%s clone %s/%s.git' % (self.git_path, self.project_root, name)
+            self.run_cmd(cmd)
+        pass
+
+
+if __name__ == '__main__':
+    AndroidSourceDownloader(root_dir=r'E:\android', tag='android-7.1.2_r33', git_path='git').main()
