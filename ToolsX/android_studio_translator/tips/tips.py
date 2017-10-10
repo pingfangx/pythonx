4f824ced10c9768f89ce1427aff1f93b23031d53
pingfangx <pingfangx@pingfangx.com>
2017-10-10 15:28:39 +0800

    不需要在 tips 中添加联系作者，将其删除

--- ToolsX/android_studio_translator/tips/tips.py
+++ ToolsX/android_studio_translator/tips/tips.py
@@ -212,9 +212,10 @@ class Tips:
                 add_cn_title = ''
             header = '<h1>[%d/%d] %s%s</h1>\n' % (i + 1, length, en_name, add_cn_title)
             if result_type == Tips.RESULT_TYPE_ANDROID_STUDIO:
-                author_url = '<a href=\'%s\'>[<font color=\'blue\'>%s</font>]</a>' % (
-                    'https://github.com/pingfangx/TranslatorX',
-                    '联系汉化作者及反馈')
+                # author_url = '<a href=\'%s\'>[<font color=\'blue\'>%s</font>]</a>' % (
+                #     'https://github.com/pingfangx/TranslatorX',
+                #     '联系汉化作者及反馈')
+                author_url = ''
                 header = '<h1>[%d/%d] %s%s %s</h1>\n' % (i + 1, length, en_name, add_cn_title, author_url)
                 footer = None
                 result_name = file_path.replace(tips_cn_dir, result_dir)
