bc9617ea811dbcb5756941f2cb98afa0f2c0a6fd
pingfangx <pingfangx@pingfangx.com>
2018-12-17 16:26:19 +0800

    [tiny_png]更新比较等方法

--- ToolsX/android/tool/tiny_png/tiny_png.py
+++ ToolsX/android/tool/tiny_png/tiny_png.py
@@ -2,6 +2,7 @@ import hashlib
 import os
 import queue
 import re
+import shutil
 
 import tinify
 
@@ -9,12 +10,14 @@ from xx import iox, filex, threadx
 
 
 class TinyPng:
-    def __init__(self, api_key, source_dir, target_dir=None, minimum_size=1024, keep_files=None, offset=0):
+    def __init__(self, api_key, source_dir, target_dir=None, old_target_dir=None, minimum_size=1024, keep_files=None,
+                 offset=0):
         """
 
         :param api_key: api key
         :param source_dir: 源目录
         :param target_dir: 目标目录，如果为空，则与源目录相同
+        :param old_target_dir: 旧版的目标目录
         :param minimum_size: 最小文件大小，小于此大小的文件不压缩
         :param keep_files:保留的文件名，可以使胙用正则匹配
         :param offset:全部压缩时的偏移
@@ -24,9 +27,14 @@ class TinyPng:
         self.target_dir = target_dir
         if not self.target_dir:
             self.target_dir = self.source_dir
+        self.old_target_dir = old_target_dir
         self.minimum_size = minimum_size
         self.keep_files = keep_files
+        if isinstance(self.keep_files, str):
+            # 如果是字符串，认为是文件
+            self.keep_files = filex.read_lines(self.keep_files, ignore_line_separator=True)
         self.offset = offset
+        self.md5_file = 'ignore/md5.txt'
 
     def main(self):
         action_list = [
@@ -38,6 +46,11 @@ class TinyPng:
             ['多线程压缩全部', self.compress_all_file_in_thread],
             ['检查压缩结果', self.check_compress_result],
             ['检查相同文件', self.check_same_file],
+            ['将结果文件复制到源目录', self.copy_target_file_to_source_dir],
+            ['-以下用来更新时使用，先切到之前的版本，生成 md5 ，再切回当前版本，复制'],
+            ['生成当前所有源文件的 md5', self.generate_md5_of_current_source_files, self.md5_file],
+            ['将当前所有源文件复制到目标目录', self.copy_source_file_to_target_dir],
+            ['根据文件 md5 复制已压缩文件', self.copy_tiny_png_by_md5, self.md5_file, self.old_target_dir],
         ]
         iox.choose_action(action_list)
 
@@ -52,29 +65,55 @@ class TinyPng:
             print(e)
 
     def list_and_print_file(self):
-        files = self.list_file()
+        files = self.list_need_compress_file()
         length = len(files)
         for i in range(self.offset, length):
             file = files[i]
-            print(f'{i+1}/{length} {filex.get_file_size_str(file)} {file}')
+            print(f'{i + 1}/{length} {filex.get_file_size_str(file)} {file}')
 
     def check_compress_result(self):
-        files = self.list_file(False)
-        self.check_files(files)
+        """检查压缩结果"""
+        self.check_files(self.list_source_image_file())
 
     def check_same_file(self):
-        files = self.list_file(False)
+        """检查源目录中的相同文件"""
+        files = self.list_source_image_file()
         md5_list = {}
         for file in files:
-            md5 = hashlib.md5(open(file, 'rb').read()).hexdigest()
+            md5 = self.get_file_md5(file)
             if md5 in md5_list.keys():
                 print(f'{file} 与 {md5_list[md5]} md5 相同，文件大小为 {filex.get_file_size_str(file)}')
             else:
                 md5_list[md5] = file
 
-    def list_file(self, ignore_compressed=True):
+    def copy_target_file_to_source_dir(self):
+        """
+        将结果文件复制到源目录
+        """
+        self.check_files(self.list_source_image_file(), move=1)
+
+    def copy_source_file_to_target_dir(self):
+        """
+        将源文件复制到目标目录
+        即认为当前源文件的所有文件是压缩后的结果，不再执行压缩
+        """
+        self.check_files(self.list_source_image_file(), move=-1)
+
+    @staticmethod
+    def get_file_md5(file_path):
+        return hashlib.md5(open(file_path, 'rb').read()).hexdigest()
+
+    def list_source_image_file(self):
+        """列出源文件"""
+        return self.list_file(self.source_dir, ignore_compressed=False)
+
+    def list_need_compress_file(self):
+        """列出需要压缩的文件"""
+        return self.list_file(self.source_dir, ignore_compressed=True)
+
+    def list_file(self, dir_path, ignore_compressed=True):
         # jpg 或 png，但不包括 .9.png
-        files = filex.list_file(self.source_dir, '(jpg)|((?<!\.9\.)png)$')
+        files = filex.list_file(dir_path, '(jpg)|((?<!\.9\.)png)$')
         print(f'过滤掉非图片文件，还剩 {len(files)} 个')
 
         files = list(filter(self.not_in_keep_files, files))
@@ -89,18 +128,43 @@ class TinyPng:
 
         return files
 
-    def check_files(self, files):
-        """检查文件"""
+    def check_files(self, files, move=0):
+        """
+        检查文件
+        :param files: 文件
+        :param move: 是否移动
+        0 不移动
+        1 将结果文件覆盖源文件
+        -1 将源文件覆盖目录文件
+        :return:
+        """
         length = len(files)
+        remain_file = 0
         for i in range(self.offset, length):
             file = files[i]
             source_file_length = os.path.getsize(file)
-            target_file_length = os.path.getsize(self.get_target_file(file))
+            target_file = self.get_target_file(file)
+            if move == -1:
+                # 为 -1 的时候是复制到目标目录
+                print(f'复制 {file} -> {target_file}')
+                self.copy_file(file, target_file)
+                continue
+            if not os.path.exists(target_file):
+                print(f'结果文件不存在 {target_file}')
+                remain_file += 1
+                continue
+            target_file_length = os.path.getsize(target_file)
 
             source_size = filex.parse_file_size(source_file_length)
             target_size = filex.parse_file_size(target_file_length)
-            reduce_size = filex.parse_file_size(source_file_length - target_file_length)
-            print(f'{i+1}/{length} {source_size} -> {target_size},减小 {reduce_size}, {file}')
+            reduce_length = source_file_length - target_file_length
+            reduce_size = filex.parse_file_size(reduce_length)
+            reduce_percent = reduce_length / source_file_length * 100
+            print(f'{i + 1}/{length} {source_size} -> {target_size},减小 {reduce_percent:.2f}%,{reduce_size}, {file}')
+            if move == 1:
+                print(f'复制 {target_file} -> {file}')
+                self.copy_file(target_file, file)
+        print(f'剩余文件 {remain_file} 个')
 
     def not_in_keep_files(self, file):
         """不是保留的文件"""
@@ -109,6 +173,7 @@ class TinyPng:
                 file_name = os.path.basename(file)
                 file_name = os.path.splitext(file_name)[0]
                 if re.fullmatch(keep_file, file_name):
+                    print(f'过滤文件 {file}')
                     return False
         return True
 
@@ -120,18 +185,18 @@ class TinyPng:
         return source_file.replace(self.source_dir, self.target_dir)
 
     def compress_first_file(self):
-        files = self.list_file()
+        files = self.list_need_compress_file()
         self.compress_image(files[0])
 
     def compress_all_file_in_loop(self):
-        files = self.list_file()
+        files = self.list_need_compress_file()
         length = len(files)
         for i in range(self.offset, length):
-            print(f'压缩每 {i+1}/{length} 张图片')
+            print(f'压缩每 {i + 1}/{length} 张图片')
             self.compress_image(files[i])
 
     def compress_all_file_in_thread(self):
-        files = self.list_file()
+        files = self.list_need_compress_file()
         length = len(files)
 
         q = queue.Queue()
@@ -167,6 +232,56 @@ class TinyPng:
         if tinify.compression_count >= 500:
             print('压缩数量已超限')
             exit()
+
+    def generate_md5_of_current_source_files(self, md5_file):
+        """生成当前所有源文件的 md5"""
+        files = self.list_source_image_file()
+        lines = []
+        for file in files:
+            md5 = self.get_file_md5(file)
+            lines.append(f'{md5},{file}')
+        filex.write_lines(md5_file, lines, add_line_separator=True)
+
+    def copy_tiny_png_by_md5(self, md5_file, reference_target_dir):
+        """
+        根据文件的 md5 复制已压缩的文件
+        如果存在 md5 ，如果在参考目标目录中有结果文件，则直接将其复制到结果目录中
+        :param md5_file: md5 文件
+        :param reference_target_dir:参考目标目录
+        :return:
+        """
+        lines = filex.read_lines(md5_file, ignore_line_separator=True)
+        if not lines:
+            print('没有 md5')
+            return
+        md5_dict = {}
+        for line in lines:
+            md5, path = line.split(',')
+            md5_dict[md5] = path
+        files = self.list_need_compress_file()
+        for file in files:
+            md5 = self.get_file_md5(file)
+            if not (md5 in md5_dict.keys()):
+                continue
+            reference_source_file = md5_dict[md5]
+            reference_target_file = reference_source_file.replace(self.source_dir, reference_target_dir)
+            if not os.path.exists(reference_target_file):
+                # 不存在参考文件
+                print(f'已压缩文件不存在 {reference_target_file}')
+                continue
+            target_file = self.get_target_file(file)
+            print(f'文件相同，复制已压缩文件 {reference_target_file} -> {target_file}')
+            self.copy_file(reference_target_file, target_file)
+
+    def copy_file(self, source, target):
+        """复制文件"""
+        filex.check_and_create_dir(target)
+        source_md5 = self.get_file_md5(source)
+        target_md5 = self.get_file_md5(target)
+        if source_md5 == target_md5:
+            print('文件已相同')
+        else:
+            shutil.copyfile(source, target)
 
 
 if __name__ == '__main__':
