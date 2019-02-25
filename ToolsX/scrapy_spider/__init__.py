022464f97685f8003a4ac99a2f7d3da4f6053917
pingfangx <pingfangx@pingfangx.com>
2019-02-25 17:21:28 +0800

    添加各包的 __init__.py 文件并添加注释

---
+++ ToolsX/scrapy_spider/__init__.py
@@ -0,0 +1,5 @@
+"""scrapy 爬虫包
+
+（要运行爬虫的时候需要以 scrapy_spider 为根目录打开项目）
+相关框架已经架构好，可以很方便地添加 spider、item、pipeline 和 middleware
+"""
---
+++ ToolsX/spider/__init__.py
@@ -0,0 +1,5 @@
+"""爬虫包
+
+使用 requests 、bs4、selenium 等工具写的小爬虫或与网络相关的小工具
+用 scrapy 写的爬虫则在 scrapy_spider 包中
+"""
