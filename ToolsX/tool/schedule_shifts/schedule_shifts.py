b8591cb9d8bdd49ad5f6079c3fa38c5fb3b33e1d
pingfangx <pingfangx@pingfangx.com>
2019-03-18 23:16:26 +0800

    [schedule_shifts]修改排班

--- ToolsX/tool/schedule_shifts/schedule_shifts.py
+++ ToolsX/tool/schedule_shifts/schedule_shifts.py
@@ -54,7 +54,7 @@ class WorkDay:
 
     def get_shifts_info(self):
         """获取本日排班信息"""
-        return f'{self}\t' + '\t'.join([shift.worker.name for shift in self.shifts])
+        return f'{self}\t' + '\t'.join(shift.worker.name for shift in self.shifts)
 
 
 class Week:
@@ -70,9 +70,20 @@ class Person:
 
     def __init__(self, name, sex=0):
         self.name = name
+        """名字"""
+
         self.sex = sex
+        """性别"""
+
         self.shift_list: List[Shift] = []
-        self.shift_times = [0] * 4
+        """已经值的班，用于记录上一次是不是夜班"""
+
+        self.pre_shift_times: List[int] = [0] * len(Shift.name_list)
+        """上一次班次的次数
+        
+        上一次班次已经不知道是星期几，只有总数
+        而且可能是累计的，所以只有数量，因此记录，用来统计班次时计算
+        """
 
     def schedule_shift(self, shift: Shift) -> bool:
         """排班
@@ -85,8 +96,6 @@ class Person:
             shift.worker = self
             # 添加排班
             self.shift_list.append(shift)
-            # 增加次数
-            self.shift_times[shift.index] += 1
             return True
         else:
             return False
@@ -104,11 +113,29 @@ class Person:
         return True
 
     def get_shift_times(self) -> int:
-        """获取班次数量"""
-        times = 0
-        for i in self.shift_times:
-            times += i
-        return times
+        """获取班次总数"""
+        return len(self.shift_list) + self.get_pre_shift_times()
+
+    def get_pre_shift_times(self):
+        """获取之前的班次总数"""
+        result = 0
+        for times in self.pre_shift_times:
+            result += times
+        return result
+
+    def get_per_shift_times(self, only_this_week: bool = False) -> List[int]:
+        """计算每一班次的数量
+
+        :param only_this_week 是否仅本周
+        """
+        result = [0] * len(Shift.name_list)
+        for shift in self.shift_list:
+            result[shift.index] += 1
+        # 加上之前的
+        if not only_this_week:
+            for i in range(len(result)):
+                result[i] += self.pre_shift_times[i]
+        return result
 
     def get_same_time_shift_times(self, shift) -> int:
         """获取相同时间段的班次的次数，用来保证每个人不同时间段平均"""
@@ -116,6 +143,8 @@ class Person:
         for scheduled_shift in self.shift_list:
             if scheduled_shift.index == shift.index:
                 times += 1
+        # 加上之前的
+        times += self.pre_shift_times[shift.index]
         return times
 
     def is_female(self) -> bool:
@@ -131,17 +160,26 @@ class Person:
 
         # 统计次数
         info += '\t'
-        info += '\t'.join(f'{Shift.name_list[i]}({times})' for i, times in enumerate(self.shift_times))
+        info += '\t'.join(f'{Shift.name_list[i]}({times})' for i, times in enumerate(self.get_per_shift_times(True)))
 
         # 每个班
         info += '\t'
-        info += '\t'.join([f'{shift.workday}{shift}' for shift in self.shift_list])
+        info += '\t'.join(f'{shift.workday}{shift}' for shift in self.shift_list)
+        return info
+
+    def get_all_shifts_info(self):
+        """获取本人把有排班信息"""
+        info = self.name
+
+        # 统计次数
+        info += '\t'
+        info += '\t'.join(f'{Shift.name_list[i]}({times})' for i, times in enumerate(self.get_per_shift_times(False)))
         return info
 
     def get_shifts_info_for_output(self):
         """用于输出的排班信息"""
         sex = '女' if self.is_female() else '男'
-        shift_times = ','.join([str(i) for i in self.shift_times])
+        shift_times = ','.join(str(i) for i in self.get_per_shift_times())
         return f"{self},{sex},{shift_times}"
 
 
@@ -165,17 +203,18 @@ class ScheduleShifts:
         # 排班
         self.schedule_all_shift()
 
+        log(f'\n\n输出排班数据: {self.shifts_output_file}')
         shifts_output = self.print_schedule_by_word_day()
         shifts_output += '\n\n' + self.print_schedule_by_person()
-        log(f'\n\n输出排班数据: {self.shifts_output_file}')
+        shifts_output += '\n\n' + self.print_all_schedule_by_person()
         log(shifts_output)
         Helper.write(shifts_output, self.shifts_output_file)
 
+        log(f'\n\n输出人员累计数据: {self.person_output_file}')
         person_output = ['# 名字,姓别,早班,午班,晚班,夜班']
         for person in self.person_list:
             person_output.append(person.get_shifts_info_for_output())
         person_output = '\n'.join(person_output)
-        log(f'\n\n输出人员数据: {self.person_output_file}')
         log(person_output)
         Helper.write(person_output, self.person_output_file)
 
@@ -189,10 +228,10 @@ class ScheduleShifts:
     def schedule_shift(self, shift: Shift):
         """排班
 
-        不支持班次肯定不排
-        如果都支持
-            如果总班数更少，排给他，因为每个人班数应该平均
-            如果部班数相同，如果该班次更少，排给他，因为各班次应该平均给每个人
+        不支持的班次不能排
+        如果支持
+            如果总班数更少，说明他少上了班，排给他，因为每个人班数应该平均
+            如果总班数相同，如果该班次更少，排给他，因为各班次应该平均给每个人
 
         :param shift:班次
         :return:
@@ -205,7 +244,22 @@ class ScheduleShifts:
                 log(f'{person} 支持此班')
                 candidate_list.append(person)
         log(f'\n共有 {len(candidate_list)} 人支持此班')
-        log(','.join([person.name for person in candidate_list]))
+        log(','.join(person.name for person in candidate_list))
+
+        log(f'\n选出值班总数少的人员')
+        min_times = 1 << 30
+        candidate_dict: Dict[int, List] = {}
+        for person in candidate_list:
+            shift_times = person.get_shift_times()
+            if shift_times in candidate_dict.keys():
+                candidate_dict[shift_times].append(person)
+            else:
+                candidate_dict[shift_times] = [person]
+            if shift_times < min_times:
+                min_times = shift_times
+        log(f'值班总数最少为 {min_times} 次，共 {len(candidate_dict[min_times])} 人')
+        candidate_list = candidate_dict[min_times]
+        log(','.join(person.name for person in candidate_list))
 
         log(f'\n选出值 {shift} 次数少的人员')
         min_times = 1 << 30
@@ -218,19 +272,13 @@ class ScheduleShifts:
                 candidate_dict[shift_times] = [person]
             if shift_times < min_times:
                 min_times = shift_times
-        log(f'值 {shift} 次数最少为 {min_times} 次，共 {len(candidate_dict[min_times])} 人都值了 {min_times} 次')
+        log(f'值 {shift} 次数最少为 {min_times} 次，共 {len(candidate_dict[min_times])} 人')
         candidate_list = candidate_dict[min_times]
-        log(','.join([person.name for person in candidate_list]))
+        log(','.join(person.name for person in candidate_list))
 
-        log(f'\n选出值班次数较少的人员')
         destination: Person = None
-        min_times = 1 << 30
-        for person in candidate_list:
-            shift_times = person.get_shift_times()
-            if shift_times < min_times:
-                min_times = shift_times
-                destination = person
-        log(f'{destination} 值班总次数较少，为 {min_times} 次')
+        if candidate_list and len(candidate_list) > 0:
+            destination = candidate_list[0]
 
         if not destination:
             log(f'\n未找到人值此班，排班失败')
@@ -245,16 +293,23 @@ class ScheduleShifts:
 
     def print_schedule_by_word_day(self):
         """按工作日输出班次"""
-        lines = ['按工作日输出班次']
+        lines = ['按工作日输出本周班次']
         for day in self.week.days:
             lines.append(day.get_shifts_info())
         return '\n'.join(lines)
 
     def print_schedule_by_person(self):
         """按人输出班次"""
-        lines = ['按人输出班次']
+        lines = ['按人输出本周班次']
         for person in self.person_list:
             lines.append(person.get_shifts_info())
+        return '\n'.join(lines)
+
+    def print_all_schedule_by_person(self):
+        """按人输出班次"""
+        lines = ['按人输出累计班次']
+        for person in self.person_list:
+            lines.append(person.get_all_shifts_info())
         return '\n'.join(lines)
 
 
@@ -295,7 +350,7 @@ class Helper:
 
                 if length == 6:
                     # 之前的排班记录
-                    person.shift_times = [int(i) for i in data[2:6]]
+                    person.pre_shift_times = [int(i) for i in data[2:6]]
                 person_list.append(person)
         return person_list
 
@@ -312,8 +367,11 @@ class Helper:
 
 if __name__ == '__main__':
     now = time.strftime('%Y%m%d%H%M%S', time.localtime())
-    _person_input = '输入人员数据.txt'
-    _person_output = f'{now}-输出人员数据.txt'
-    _shifts_output = f'{now}-排班结果.txt'
-    Helper.log_file = f'{now}-排班过程.txt'
+    _output_dir = 'output/'
+    if not os.path.exists(_output_dir):
+        os.makedirs(_output_dir)
+    _person_input = '人员数据.txt'
+    _person_output = f'{_output_dir}/{now}-输出人员累计数据.txt'
+    _shifts_output = f'{_output_dir}/{now}-排班结果.txt'
+    Helper.log_file = f'{_output_dir}/{now}-排班过程.txt'
     ScheduleShifts(_person_input, _person_output, _shifts_output).schedule()
