e30821fac7d5d7db1be6046460355d0b16fd9bc3
pingfangx <pingfangx@pingfangx.com>
2019-10-12 17:59:20 +0800

    [omegat]添加替换特殊字符的方法

--- ToolsX/tool/translatorx/omegat/tmx/tmx_utils.py
+++ ToolsX/tool/translatorx/omegat/tmx/tmx_utils.py
@@ -3,11 +3,23 @@ import re
 from twisted.trial import unittest
 
 
+def replace_special_word(file, output):
+    edit_file(file, output, lambda x: x.replace("'\x02apos;", "'\\u002E'"))
+
+
 def update_from_level2(file, output):
-    with open(file, encoding='utf-8') as f:
-        text = f.read()
+    def m(text: str) -> str:
         text = text.replace('xml:lang', 'lang')  # 替换语言标签
         text = re.sub(r'<[be]pt.*?/>', '', text)  # 替换 bpt 和 ept
+        return text
+
+    edit_file(file, output, m)
+
+
+def edit_file(file, output, callback):
+    with open(file, encoding='utf-8') as f:
+        text = f.read()
+        text = callback(text)
         with open(output, 'w', encoding='utf-8') as of:
             of.write(text)
             print(f'写入 {output}')
@@ -39,6 +51,12 @@ def replace_shortcut_tag(actual, en, cn):
 
 
 class _Test(unittest.TestCase):
+    def test_replace_special_word(self):
+        replace_special_word(
+            r'',
+            r'',
+        )
+
     def test_update_from_level2(self):
         update_from_level2(
             file=r'D:\workspace\TranslatorX-other\JavaDocs\tm\Java-api-1.6.tmx',
